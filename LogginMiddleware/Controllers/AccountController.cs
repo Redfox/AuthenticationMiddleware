9211d7103ddd7bd95f68d09c024a7386ad3357c4
unknown <vitor_pereira.2013@hotmail.com>
2019-05-18 14:05:55 -0300

    initial commit

---
+++ LogginMiddleware/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace LogginMiddleware.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Nome, string Senha)
+        {
+            if (Nome == null && Senha == null)
+                return View("Index");
+
+            HttpContext.Session.SetString("Nome", Nome);
+            return View("Welcome");
+        }
+    }
+}
